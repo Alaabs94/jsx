d40bff91bb721fd95a05fa8dfafc5cfea003367b
unknown <alaabs.94@gmail.com>
2022-05-24 18:52:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import image2 from "./image2.jpg"
+// import attack1 from "./videos/attack1.mp4"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1 class="title">EREN YEAGER</h1>
     </div>
+    <div>
+<img className='img2'  src={image2} alt="image2"/>
+    </div>
+    <div>
+<img className='img1' src="/image1.jpg" alt="image1"/>
+    </div>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/1q2_A7y4QDw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </>
   );
 }
 
